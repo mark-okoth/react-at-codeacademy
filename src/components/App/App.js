a1b09c4649cd308d130df5d04a3854bc2ae6d08d
mark odhiambo okoth <markokoth96@gmail.com>
2020-04-25 20:16:19 +0300

    working on the search component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,40 @@
 import React, {Component} from 'react';
 import './App.css';
-
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+const business = {
+  id:1,
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
 class App extends Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList />
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
